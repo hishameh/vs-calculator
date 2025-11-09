e02305b7016c178e3b75f8aa4faa6a8018f77219
hishameh <109773901+hishameh@users.noreply.github.com>
2025-11-09 15:47:18 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,18 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import ArchitectFee from "./pages/ArchitectFee";
+import ArchitectFeeCalculator from "./components/ArchitectFeeCalculator";
 import NotFound from "./pages/NotFound";
-import CursorAnimation from "./components/CursorAnimation";
-import ArchitectFeeCalculator from './pages/ArchitectFeeCalculator';
-
-// In your Routes component:
-<Routes>
-  <Route path="/" element={<Index />} />
-  <Route path="/architect-fee" element={<ArchitectFeeCalculator />} />
-  <Route path="*" element={<NotFound />} />
-</Routes>
-const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
+  <BrowserRouter>
     <div className="bg-[#f0ede8] min-h-screen">
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <CursorAnimation />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/architect-fee" element={<ArchitectFee />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/architect-fee" element={<ArchitectFeeCalculator />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
-  </QueryClientProvider>
+  </BrowserRouter>
 );
 
 export default App;
